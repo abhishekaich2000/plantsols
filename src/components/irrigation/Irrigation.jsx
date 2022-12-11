9c65d6abb53d6808f4977e8b63b3a7c065663bd4
Abhishek Aich <63039981+abhishekaich2000@users.noreply.github.com>
2022-12-11 09:10:27 +0530

    tamil lang added in irrigation jsx

--- src/components/irrigation/Irrigation.jsx
+++ src/components/irrigation/Irrigation.jsx
@@ -6,6 +6,8 @@ export default function Irrigation() {
     <div className="container irrigation">
       <h3 className='my-4'>Drip irrigation</h3>
       <p>Drip irrigation or trickle irrigation is a type of micro-irrigation system that has the potential to save water and nutrients by allowing water to drip slowly to the roots of plants, either from above the soil surface or buried below the surface.</p>
+
+      <p>சொட்டு நீர் பாசனம் அல்லது துளிர் பாசனம் என்பது ஒரு வகை நுண்ணீர் பாசன அமைப்பாகும், இது மண்ணின் மேற்பரப்பிலிருந்து அல்லது மேற்பரப்பிற்கு கீழே புதைக்கப்பட்ட தாவரங்களின் வேர்களுக்கு மெதுவாக நீர் சொட்ட அனுமதிப்பதன் மூலம் நீர் மற்றும் ஊட்டச்சத்துக்களை சேமிக்கும் திறன் கொண்டது.</p>
     
       <h4>Advantages of Drip Irrigation:
       </h4>
@@ -18,16 +20,43 @@ export default function Irrigation() {
         <li>Low labour and relatively low operation cost.</li>
         <li>No soil erosion.</li>
       </ul>
+
+      <h4>சொட்டு நீர் பாசனத்தின் நன்மைகள்:</h4>
+      <ul>
+        <li>கிடைக்கும் நீரின் அதிகபட்ச பயன்பாடு.</li>
+        <li>களைகளுக்கு தண்ணீர் இல்லை.</li>
+        <li>அதிகபட்ச பயிர் மகசூல்.</li>
+        <li>உரங்களின் பயன்பாட்டில் அதிக திறன்.</li>
+        <li>குறைந்த களை வளர்ச்சி மற்றும் சாத்தியமான புரவலன்களின் எண்ணிக்கையை கட்டுப்படுத்துகிறது.</li>
+        <li>குறைந்த உழைப்பு மற்றும் ஒப்பீட்டளவில் குறைந்த செயல்பாட்டு செலவு.</li>
+        <li>மண் அரிப்பு இல்லை.</li>
+      </ul>
+
       <p>Drip irrigation is <b>the most efficient water and nutrient delivery system for growing crops</b>. It delivers water and nutrients directly to the plant's roots zone, in the right amounts, at the right time, so each plant gets exactly what it needs, when it needs it, to grow optimally.
       </p>
+
+      <p>சொட்டு நீர் பாசனம் பயிர்களை வளர்ப்பதற்கு மிகவும் திறமையான நீர் மற்றும் ஊட்டச்சத்து விநியோக முறையாகும். இது நீர் மற்றும் ஊட்டச்சத்துக்களை நேரடியாக தாவரத்தின் வேர் மண்டலத்திற்கு, சரியான அளவுகளில், சரியான நேரத்தில் வழங்குகிறது, எனவே ஒவ்வொரு தாவரமும் தனக்குத் தேவையானதை, தேவைப்படும்போது, ​​உகந்ததாக வளரச் செய்கிறது.
+      </p>
+
       <p>Drip irrigation can use either <b>PVC pipe or polyethylene tubing</b> (or a combination of both). PVC systems are usually reserved for larger‐scale applications (like farming), while polyethylene tubing is the choice for home gardening.</p>
+
+      <p>சொட்டு நீர் பாசனத்தில் PVC குழாய் அல்லது பாலிஎதிலீன் குழாய் (அல்லது இரண்டின் கலவையும்) பயன்படுத்தலாம். PVC அமைப்புகள் பொதுவாக பெரிய அளவிலான பயன்பாடுகளுக்கு (விவசாயம் போன்றவை) ஒதுக்கப்படுகின்றன, அதே நேரத்தில் பாலிஎதிலீன் குழாய்கள் வீட்டுத் தோட்டக்கலைக்கான தேர்வாகும்.</p>
 
       <h4>Porous Soaker Hose</h4>
       <p>This is one of the best drip irrigation methods to use to get water to rows of shrubs or plant beds. It produces the least among of evaporation because it allows the soil to absorb all of the water it releases. It directs the water to plant roots.</p>
       <p>Drip runs longer than other irrigation types — <b>45-60-minute</b> run times are not uncommon — so any leaks are magnified over a long run cycle.</p>
+
+      <h4>நுண்துளை சோக்கர் குழாய்</h4>
+      <p>புதர்கள் அல்லது தாவரப் பாத்திகளின் வரிசைகளுக்குத் தண்ணீரைப் பெறுவதற்கு இது சிறந்த சொட்டு நீர் பாசன முறைகளில் ஒன்றாகும். இது குறைந்த ஆவியாதல் உற்பத்தி செய்கிறது, ஏனெனில் அது வெளியிடும் அனைத்து நீரையும் மண் உறிஞ்சுவதற்கு அனுமதிக்கிறது. இது தாவர வேர்களுக்கு தண்ணீரை வழிநடத்துகிறது.</p>
+      <p>மற்ற நீர்ப்பாசன வகைகளை விட சொட்டுநீர் நீண்ட நேரம் ஓடுகிறது - 45-60 நிமிட ஓட்ட நேரங்கள் அசாதாரணமானது அல்ல - எனவே எந்த கசிவும் நீண்ட கால சுழற்சியில் பெரிதாக்கப்படுகிறது.</p>
+
       <p><b>Drip irrigation systems will use less water and reduce waste</b>. Saving water and reducing consumption is important these days, and if that is high up on your list as well, then you may want to go with a drip irrigation system.
       </p>
       <p>Drip irrigation prevents overwatering. The system consists of interconnected drip irrigation tools such as emitters, hoses and valves. An emitter will consistently provide water to a plant's roots. Laying these tools out correctly will give the best results.</p>
+
+      <p>சொட்டு நீர் பாசன முறைகள் குறைந்த நீரை பயன்படுத்தி கழிவுகளை குறைக்கும். இந்த நாட்களில் தண்ணீரைச் சேமிப்பதும் நுகர்வைக் குறைப்பதும் முக்கியம், அதுவும் உங்கள் பட்டியலில் அதிகமாக இருந்தால், நீங்கள் சொட்டு நீர்ப் பாசன முறையைப் பயன்படுத்த விரும்பலாம்.</p>
+      <p>சொட்டு நீர் பாசனம் அதிகப்படியான நீர்ப்பாசனத்தை தடுக்கிறது. இந்த அமைப்பு உமிழ்ப்பான்கள், குழல்களை மற்றும் வால்வுகள் போன்ற ஒன்றோடொன்று இணைக்கப்பட்ட சொட்டு நீர் பாசன கருவிகளைக் கொண்டுள்ளது. ஒரு உமிழ்ப்பான் ஒரு தாவரத்தின் வேர்களுக்கு தொடர்ந்து தண்ணீரை வழங்கும். இந்த கருவிகளை சரியாக அடுக்கி வைப்பது சிறந்த பலனைத் தரும்.
+      </p>
 
       <img className='my-3' src="http://drive.google.com/uc?export=view&id=1_RSQeRCoUf_k1DED8naq8aneDvMaZYrn" alt="" />
       <img className='my-3 mx-4' src="http://drive.google.com/uc?export=view&id=1JyUoIUfLRP4k5GOLBU5tRSQd-9Or_4SS" alt="" />
@@ -38,16 +67,29 @@ export default function Irrigation() {
       <h3 className='my-4'>Sprinkler irrigation</h3>
       <p>Sprinkler/spray irrigation is the method of applying water to a controlled manner in that is similar to rainfall. The water is distributed through a network that may consist of pumps, valves, pipes, and sprinklers. Irrigation sprinklers can be used for residential, industrial, and agricultural usage.</p>
 
+      <p>தெளிப்பான்/தெளிப்பு நீர்ப்பாசனம் என்பது மழையைப் போன்றே கட்டுப்படுத்தப்பட்ட முறையில் தண்ணீரைப் பயன்படுத்துவதற்கான முறையாகும். குழாய்கள், வால்வுகள், குழாய்கள் மற்றும் தெளிப்பான்கள் ஆகியவற்றைக் கொண்ட நெட்வொர்க் மூலம் தண்ணீர் விநியோகிக்கப்படுகிறது. நீர்ப்பாசன தெளிப்பான்கள் குடியிருப்பு, தொழில்துறை மற்றும் விவசாய பயன்பாட்டிற்கு பயன்படுத்தப்படலாம்.
+      </p>
+
       <img className='my-3' src="http://drive.google.com/uc?export=view&id=1MU-iEYkhmEvo8sElR0iXKbEPsUEWuGaZ" alt="" />
       <p>Sprinkler irrigation is widely used because <b>it is versatile, adaptable to almost all crops, and affordable</b>. But it's not a perfect irrigation system. Spray irrigation is one of the most inefficient watering methods, wasting approximately 30 to 50% of the water applied through run-off and evaporation.</p>
 
       <p>Sprinkler irrigation is also called <b>overhead irrigation</b>. In this method of irrigation water is applied in the form of a spray or artificial rain. Water is sprayed into the air and allowed to fall on the ground surface somewhat resembling rainfall.</p>
 
+      <p>தெளிப்பு நீர்ப்பாசனம் பரவலாகப் பயன்படுத்தப்படுகிறது, ஏனெனில் இது பல்துறை, கிட்டத்தட்ட அனைத்து பயிர்களுக்கும் பொருந்தக்கூடியது மற்றும் மலிவானது. ஆனால் இது சரியான நீர்ப்பாசன முறை அல்ல. தெளிப்பு நீர்ப்பாசனம் மிகவும் திறமையற்ற நீர்ப்பாசன முறைகளில் ஒன்றாகும், இது ரன்-ஆஃப் மற்றும் ஆவியாதல் மூலம் பயன்படுத்தப்படும் நீரில் சுமார் 30 முதல் 50% வரை வீணாகிறது.</p>
+      <p>தெளிப்பு நீர்ப்பாசனம் மேல்நிலை நீர்ப்பாசனம் என்றும் அழைக்கப்படுகிறது. இந்த பாசன முறையில் நீர் தெளிப்பு அல்லது செயற்கை மழை வடிவில் பயன்படுத்தப்படுகிறது. நீர் காற்றில் தெளிக்கப்பட்டு, மழைப்பொழிவை ஒத்த நிலத்தின் மேற்பரப்பில் விழ அனுமதிக்கப்படுகிறது.</p>
+
       <p>Sprinkler irrigation is suited for <b>most row, field and tree crops</b> and water can be sprayed over or under the crop canopy. However, large sprinklers are not recommended for irrigation of delicate crops such as lettuce because the large water drops produced by the sprinklers may damage the crop.</p>
 
       <p>Sprinkler system : This system is used on the uneven land where less water is available.</p>
 
+      <p>பெரும்பாலான வரிசை, வயல் மற்றும் மரப் பயிர்களுக்கு தெளிப்பு நீர்ப்பாசனம் மிகவும் பொருத்தமானது மற்றும் பயிர் விதானத்தின் மேல் அல்லது கீழ் நீர் தெளிக்கலாம். இருப்பினும், கீரை போன்ற மென்மையான பயிர்களுக்கு நீர்ப்பாசனம் செய்ய பெரிய தெளிப்பான்கள் பரிந்துரைக்கப்படுவதில்லை, ஏனெனில் தெளிப்பான்களால் உற்பத்தி செய்யப்படும் பெரிய நீர்த்துளிகள் பயிரை சேதப்படுத்தும்.</p>
+
+      <p>தெளிப்பான் அமைப்பு: இந்த அமைப்பு குறைந்த நீர் கிடைக்கும் சமமற்ற நிலத்தில் பயன்படுத்தப்படுகிறது.</p>
+
       <p>Micro Irrigation technology plays a vital role in Agriculture in saving water and increasing productivity through effective utilization of every drop of water. Micro Irrigation has received considerable attention from farmers for its perceived ability to contribute significantly to groundwater resources development, agricultural productivity and economic growth. Drip Irrigation System is a life saver for many farmers in Tamil Nadu, by enhancing the yield and quality of the produce. Increasing water scarcity and limited availability of labour has paved the way for implementation of Micro Irrigation scheme. Micro Irrigation technology improves water use efficiency by 40% - 60% by precise water application. Through fertigation technology, fertilizers are directly applied to the root zone in drip irrigation, hence fertilizer use efficiency is also increased.</p>
+
+      <p>ஒவ்வொரு சொட்டு நீரையும் திறம்பட பயன்படுத்துவதன் மூலம் நீரைச் சேமிப்பதிலும் உற்பத்தித்திறனை அதிகரிப்பதிலும் விவசாயத்தில் நுண்ணீர்ப்பாசனத் தொழில்நுட்பம் முக்கிய பங்கு வகிக்கிறது. நிலத்தடி நீர்வள மேம்பாடு, விவசாய உற்பத்தித்திறன் மற்றும் பொருளாதார வளர்ச்சி ஆகியவற்றில் குறிப்பிடத்தக்க பங்களிப்பை வழங்கும் திறனுக்காக நுண்ணீர் பாசனம் விவசாயிகளிடமிருந்து கணிசமான கவனத்தைப் பெற்றுள்ளது. சொட்டு நீர் பாசன முறையானது, விளைச்சலின் விளைச்சலையும் தரத்தையும் அதிகரிப்பதன் மூலம், தமிழகத்தில் உள்ள பல விவசாயிகளின் உயிர் காக்கும். அதிகரித்து வரும் தண்ணீர் பற்றாக்குறை மற்றும் குறைந்த அளவிலான தொழிலாளர் இருப்பு ஆகியவை நுண்ணீர் பாசன திட்டத்தை செயல்படுத்த வழிவகை செய்துள்ளது. நுண்ணீர் பாசன தொழில்நுட்பம், துல்லியமான நீர் பயன்பாட்டினால் நீர் பயன்பாட்டு திறனை 40% - 60% மேம்படுத்துகிறது. உரமிடுதல் தொழில்நுட்பத்தின் மூலம், சொட்டு நீர் பாசனத்தில் உரங்கள் நேரடியாக வேர் மண்டலத்தில் பயன்படுத்தப்படுகின்றன, எனவே உர பயன்பாட்டு திறன் அதிகரிக்கிறது.
+      </p>
 
       <p>More about drip irrigation - <a  target="_blank" rel="noopener noreferrer" href="https://tnhorticulture.tn.gov.in/micro
       ">Open</a></p>
